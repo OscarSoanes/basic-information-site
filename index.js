a2a4009842fc09a1d22dc788c397e8f24b890196
Oscar Soanes <oscarsoanes@gmail.com>
2023-06-01 14:56:44 +0100

    Basic router working with three main sites

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
 const http = require("http");
-const URL = require("url")
+const URL = require("url");
+const fs = require("fs");
 
 http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  
   const path = req.url
   const view = getViewFromURL(path);
 
-  console.log(view);
+  fs.readFile(view, (err, dat) => {
+    if (err) throw err;
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(dat);
+    return res.end;
 
-  res.end("");
+  })
 }).listen(8080)
 
 function getViewFromURL (path) {
   switch (path) {
-    case "/": return "index.html";
-    case "/contact": return "contact.html";
-    case "/about": return "about.html";
+    case "/": return "views/index.html";
+    case "/contact": return "views/contact.html";
+    case "/about": return "views/about.html";
+    case "/favicon.ico": return;
     default: return;
   }
 }
