21a880e4538059afe41e61c40a4aaa4c8c89eb2e
Oscar Soanes <oscarsoanes@gmail.com>
2023-06-01 14:49:01 +0100

    Basic ability to get the view

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const http = require("http");
+const URL = require("url")
 
 http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end();
+  
+  const path = req.url
+  const view = getViewFromURL(path);
+
+  console.log(view);
+
+  res.end("");
 }).listen(8080)
+
+function getViewFromURL (path) {
+  switch (path) {
+    case "/": return "index.html";
+    case "/contact": return "contact.html";
+    case "/about": return "about.html";
+    default: return;
+  }
+}
